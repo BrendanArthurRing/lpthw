8766b18568158893f2f3f29725cf6b5ec89f6d3e
blkrnt <blkrnt@gmail.com>
2020-02-26 09:27:55 +0900

    renamed using 0 in the titles

---
+++ exercise_01.py
@@ -0,0 +1,9 @@
+# Exercise 1 from LPTHW by Zed A. Shaw
+
+print("Hello World!")
+print("Hello again.")
+print("I like typing this")
+print("This is fun.")
+print("Yay! Printing!")
+print("I'd much rather you not.")
+print('I "said" do not touch this.')
